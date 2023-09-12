64b93cc74cdd7e374dfbad5d6d0b394815daaad9
rlatkdgns <rlatkdgns042@naver.com>
2023-09-12 19:42:43 +0900

    renamed:    2023_09/leecote/001_bs_1.py -> 2023_09/leecote/001_bisect_1.py 	renamed:    2023_09/leecote/002_bs_2.py -> 2023_09/leecote/002_bisect_2.py 	new file:   2023_09/leecote/003_DP_1.py 	new file:   2023_09/leecote/004_DP_2.py

---
+++ 2023_09/leecote/003_DP_1.py
@@ -0,0 +1,16 @@
+import sys
+read = sys.stdin.readline
+
+n = int(read())
+k = list(map(int, read().split()))
+
+
+d = [0] * 100
+
+d[0] = k[0]
+d[1] = max(k[0],k[1])
+
+for i in range(2, n):
+  d[i] = max(d[i-1], d[i-2]+k[i])
+
+print(d[n-1])
