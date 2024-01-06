96ae6b42a1f2bd02e91e22e249247298748c94f8
Sanghun Kim <rlatkdgns042@naver.com>
2024-01-06 12:27:31 +0900

    [level 1] Title: 공원 산책, Time: 0.13 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/172928. 공원 산책/공원 산책.py
@@ -0,0 +1,41 @@
+def solution(park, routes):
+    x = 0
+    y = 0 
+
+    for i in range(len(park)):
+        for j in range(len(park[i])):
+            if park[i][j] == 'S':
+                x = j
+                y = i
+                break
+    
+    for route in routes:
+        xx = x
+        yy = y
+
+        for step in range(int(route[2])):
+            if route[0] == 'E' and xx != len(park[0])-1 and park[yy][xx+1] != 'X':
+                xx += 1
+                
+                if step == int(route[2])-1:
+                    x = xx
+
+            elif route[0] == 'W' and xx != 0 and park[yy][xx-1] != 'X':
+                xx -= 1
+                
+                if step == int(route[2])-1:
+                    x = xx
+
+            elif route[0] == 'S' and yy != len(park)-1 and park[yy+1][xx] != 'X':
+                yy += 1
+                
+                if step == int(route[2])-1:
+                    y = yy
+
+            elif route[0] == 'N' and yy != 0 and park[yy-1][xx] != 'X':
+                yy -= 1
+                
+                if step == int(route[2])-1:
+                    y = yy
+
+    return [y, x]
